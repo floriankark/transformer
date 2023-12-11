565b6189b4f8da97938728a44be0e78d8e38fba2
floriankark <florian.kark@gmail.com>
2023-12-11 12:11:58 +0100

    implement mask for future tokens

--- modelling/attention.py
+++ modelling/attention.py
@@ -5,8 +5,13 @@ import numpy as np
 
 
 class Attention(nn.Module):
-    def __init__(self):
+    def __init__(self, mask_future=False):
         super().__init__()
 
-    def forward(self, q, k, v, mask=None):
+    def forward(self, q, k, v):
+        mask = (
+            -1e10 * torch.triu(torch.ones(q.size(1), k.size(1)), diagonal=1)
+            if self.mask
+            else 0
+        )  # upper triangular matrix [n_q, n_k] -∞
         return F.softmax(q @ k.T / np.sqrt(q.size(-1)) + mask, dim=-1) @ v
