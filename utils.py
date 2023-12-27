6a8c907f050406712c5b74450836afc953f88011
floriankark <florian.kark@gmail.com>
2023-12-27 17:54:35 +0100

    add BPETokenizer structure

---
+++ utils.py
@@ -0,0 +1,10 @@
+class BPETokenizer:
+    def __init__(self, corpus, vocab_size):
+        self.corpus = corpus
+        self.vocab_size = vocab_size
+
+    def train(self):
+        pass
+
+    def tokenize(self):
+        pass
