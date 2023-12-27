2f5e7f58aee09feb12733bd3ee71082f6d1f29b9
floriankark <florian.kark@gmail.com>
2023-12-27 15:22:58 +0100

    final scaled dot-product attention

--- modelling/attention.py
+++ modelling/attention.py
@@ -7,11 +7,24 @@ import numpy as np
 class Attention(nn.Module):
     def __init__(self, mask_future=False):
         super().__init__()
+        self.mask_future = mask_future
 
-    def forward(self, q, k, v):
-        mask = (
-            -1e10 * torch.triu(torch.ones(q.size(1), k.size(1)), diagonal=1)
-            if self.mask
-            else 0
-        )  # upper triangular matrix [n_q, n_k] -∞
-        return F.softmax(q @ k.T / np.sqrt(q.size(-1)) + mask, dim=-1) @ v
+    def forward(self, q, k, v, attention_mask=None):
+        mask = torch.zeros(q.size(1), k.size(1))
+        if self.mask_future:
+            mask = mask.masked_fill(
+                torch.triu(torch.ones(mask.shape), diagonal=1) == 1, float(-1e10)
+            )
+
+        mask = torch.stack([mask] * q.size(0))
+        mask = mask.masked_fill(attention_mask.unsqueeze(1) == 0, float(-1e10))
+
+        attention = torch.matmul(
+            F.softmax(
+                torch.matmul(q, k.transpose(1, 2)) / np.sqrt(q.size(-1)) + mask,
+                dim=-1,
+            ),
+            v,
+        )
+
+        return attention
