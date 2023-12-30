ebcbfe7cc6f3ab194e60c6bd95db26fc7ac693a5
floriankark <florian.kark@gmail.com>
2023-12-30 16:35:43 +0100

    implement data preprocessing and cleaning

---
+++ dataset.py
@@ -0,0 +1,53 @@
+from datasets import load_dataset
+import re
+
+dataset = load_dataset("wmt17", "de-en")
+
+URL_PATTERN = re.compile(r"http\S+")
+TAG_PATTERN = re.compile(r"<.*?>")
+
+
+def clean_text(text):
+    text = text.encode("utf-8").decode("utf-8")
+    text = URL_PATTERN.sub("", text)
+    text = TAG_PATTERN.sub("", text)
+    whitelist = set(
+        "abcdefghijklmnopqrstuvwxyz ÄÖÜäöüß ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.,!?()[]{}:;-&$@#%£€/\\|_+*¥"
+    )
+    text = "".join(char.lower() for char in text if char in whitelist)
+    return text
+
+
+def filter_by_length(source, target, min_length, max_length):
+    return (
+        min_length <= len(source.split()) <= max_length
+        and min_length <= len(target.split()) <= max_length
+    )
+
+
+def filter_by_ratio(source, target, ratio):
+    return len(source.split()) / len(target.split()) < ratio
+
+
+def preprocess(example, min_length=5, max_length=64, ratio=1.5):
+    source = clean_text(example["de"])
+    target = clean_text(example["en"])
+    if filter_by_length(source, target, min_length, max_length) and filter_by_ratio(
+        source, target, ratio
+    ):
+        example["de"] = source
+        example["en"] = target
+        return example
+    else:
+        return None
+
+
+cleaned_dataset = dataset["train"].map(
+    lambda example: {"translation": preprocess(example["translation"], 5, 64, 1.5)},
+    num_proc=8,
+)
+cleaned_dataset = cleaned_dataset.filter(
+    lambda example: example["translation"] is not None
+)
+
+cleaned_dataset.save_to_disk("data/wmt17_de_en")
