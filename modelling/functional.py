e7e05b90912ccc3b5ce37e8834fd72446343dec5
floriankark <florian.kark@gmail.com>
2024-01-05 16:47:22 +0100

    Implement TransformerDecoderLayer

--- modelling/functional.py
+++ modelling/functional.py
@@ -40,3 +40,55 @@ class BaseTransformerLayer(nn.Module):
         )
         x = self.layer_norm_2(x + self.dropout(self.feature_transformation(x)))
         return x
+
+
+class TransformerDecoderLayer(nn.Module):
+    def __init__(
+        self,
+        input_dim,
+        num_heads,
+        feature_dim,
+        dropout=0.1,
+        mask_future=False,
+        bias=False,
+    ):
+        super().__init__()
+        self.dropout = nn.Dropout(dropout)
+
+        self.self_attention = MultiHeadAttention(
+            input_dim, num_heads, mask_future=True, bias=bias
+        )
+        self.layer_norm_1 = nn.LayerNorm(input_dim)
+
+        self.encoder_attention = MultiHeadAttention(
+            input_dim, num_heads, mask_future, bias
+        )
+        self.layer_norm_2 = nn.LayerNorm(input_dim)
+
+        self.feature_transformation = nn.Sequential(
+            OrderedDict(
+                [
+                    ("linear1", nn.Linear(input_dim, feature_dim)),
+                    ("relu", nn.ReLU()),
+                    ("linear2", nn.Linear(feature_dim, input_dim)),
+                ]
+            )
+        )
+        self.layer_norm_3 = nn.LayerNorm(input_dim)
+
+    def forward(
+        self, x, encoder_output, encoder_attention_mask=None, attention_mask=None
+    ):
+        x = self.layer_norm_1(
+            x + self.dropout(self.self_attention(x, x, x, attention_mask))
+        )
+        x = self.layer_norm_2(
+            x
+            + self.dropout(
+                self.encoder_attention(
+                    x, encoder_output, encoder_output, encoder_attention_mask
+                )
+            )
+        )
+        x = self.layer_norm_3(x + self.dropout(self.feature_transformation(x)))
+        return x
