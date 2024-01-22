5c3c887124494b7ce82c116730fa9c8f62d77f17
floriankark <florian.kark@gmail.com>
2024-01-22 12:31:54 +0100

    add TransformerLRScheduler

---
+++ modelling/lr_scheduler.py
@@ -0,0 +1,16 @@
+import torch
+from torch.optim import Optimizer
+from torch.optim.lr_scheduler import LambdaLR
+
+
+# inherit from LambdaLR to get get_last_lr() and step() methods for free
+class TransformerLRScheduler(LambdaLR):
+    def __init__(self, optimizer: Optimizer, d_model, warmup_steps=4000):
+        self.d_model = d_model
+        self.warmup_steps = warmup_steps
+        super(TransformerLRScheduler, self).__init__(optimizer, self.lr_lambda)
+
+    def lr_lambda(self, step):
+        arg1 = step ** (-0.5)
+        arg2 = step * self.warmup_steps ** (-1.5)
+        return self.d_model ** (-0.5) * min(arg1, arg2)
